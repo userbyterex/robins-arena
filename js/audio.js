cb93ecc3ccc2cff719877ec4dd9224cc37b40978
byteRexTech <userbyterex@gmail.com>
2026-07-27 22:43:03 +0200

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,21 +1,26 @@
 /**
  * audio.js
- * Efectos de sonido generados con osciladores (Web Audio API). Cero archivos
- * de audio que descargar o que puedan faltar al desplegar en GitHub Pages.
+ * Efectos de sonido generados con osciladores (Web Audio API).
  */
-const AudioFX = (() => {
-  let ctx = null;
+var AudioFX = (function () {
+  var ctx = null;
 
   function getCtx() {
     if (!ctx) ctx = new (window.AudioContext || window.webkitAudioContext)();
     return ctx;
   }
 
-  function beep({ freq = 440, duration = 0.1, type = "square", volume = 0.15, slideTo = null }) {
+  function beep(opts) {
+    opts = opts || {};
+    var freq = opts.freq || 440;
+    var duration = opts.duration || 0.1;
+    var type = opts.type || "square";
+    var volume = opts.volume || 0.15;
+    var slideTo = opts.slideTo || null;
     try {
-      const ac = getCtx();
-      const osc = ac.createOscillator();
-      const gain = ac.createGain();
+      var ac = getCtx();
+      var osc = ac.createOscillator();
+      var gain = ac.createGain();
       osc.type = type;
       osc.frequency.setValueAtTime(freq, ac.currentTime);
       if (slideTo) osc.frequency.exponentialRampToValueAtTime(slideTo, ac.currentTime + duration);
@@ -25,14 +30,14 @@ const AudioFX = (() => {
       gain.connect(ac.destination);
       osc.start();
       osc.stop(ac.currentTime + duration);
-    } catch (e) { /* audio no disponible, silenciosamente ignorar */ }
+    } catch (e) {}
   }
 
   return {
-    meleeSwing: () => beep({ freq: 180, duration: 0.08, type: "triangle", slideTo: 90 }),
-    shoot: () => beep({ freq: 500, duration: 0.12, type: "sawtooth", slideTo: 200 }),
-    hit: () => beep({ freq: 300, duration: 0.09, type: "square", slideTo: 120, volume: 0.2 }),
-    death: () => beep({ freq: 220, duration: 0.4, type: "sawtooth", slideTo: 40, volume: 0.25 }),
-    resume: () => { try { getCtx().resume(); } catch (e) {} },
+    meleeSwing: function () { beep({ freq: 180, duration: 0.08, type: "triangle", slideTo: 90 }); },
+    shoot: function () { beep({ freq: 500, duration: 0.12, type: "sawtooth", slideTo: 200 }); },
+    hit: function () { beep({ freq: 300, duration: 0.09, type: "square", slideTo: 120, volume: 0.2 }); },
+    death: function () { beep({ freq: 220, duration: 0.4, type: "sawtooth", slideTo: 40, volume: 0.25 }); },
+    resume: function () { try { getCtx().resume(); } catch (e) {} },
   };
 })();
